e146f925589f7e9c49487f8b8ce195b4e5c0241b
Edgar Toll <edjopato@gmail.com>
2019-03-29 18:13:52 +0100

    feat(construction): show upgrade cost and calc upgrade

---
+++ source/lib/interface/resource.ts
@@ -0,0 +1,38 @@
+import {
+	CONSTRUCTION_RESOURCES,
+	ConstructionResources,
+	EMOJI,
+	ResourceName,
+	RESOURCES,
+	Resources
+} from 'bastion-siege-logic'
+
+export function resourceLine(ctx: any, resource: ResourceName, amount: number): string {
+	const parts: string[] = []
+
+	parts.push(EMOJI[resource])
+	parts.push(ctx.wd.label(`resource.${resource}`))
+	parts.push(String(amount))
+
+	return parts.join(' ')
+}
+
+export function constructionResourceLine(ctx: any, resource: ResourceName, amount: number, possible: boolean): string {
+	const prefix = possible ? '✅' : '⛔️'
+	return `${prefix} ${resourceLine(ctx, resource, amount)}`
+}
+
+export function resources(ctx: any, resources: Resources): string {
+	const lines = RESOURCES
+		.map(o => resourceLine(ctx, o, resources[o]))
+
+	return lines.join('\n')
+}
+
+export function constructionResources(ctx: any, required: ConstructionResources, available: Resources): string {
+	const lines = CONSTRUCTION_RESOURCES
+		.filter(o => required[o])
+		.map(o => constructionResourceLine(ctx, o, required[o], available[o] >= required[o]))
+
+	return lines.join('\n')
+}
--- source/menu/construction.ts
+++ source/menu/construction.ts
@@ -1,14 +1,22 @@
 import TelegrafInlineMenu from 'telegraf-inline-menu'
-import {ConstructionName, CONSTRUCTIONS} from 'bastion-siege-logic'
+import {
+	calcBuildingCost,
+	calcMinutesNeeded,
+	calcResourcesAfterConstruction,
+	ConstructionName,
+	Constructions,
+	Resources
+} from 'bastion-siege-logic'
 
 import {infoHeader} from '../lib/interface/construction'
+import {constructionResources} from '../lib/interface/resource'
 
 const menu = new TelegrafInlineMenu(constructionText)
 
 function constructionFromCtx(ctx: any): {construction: ConstructionName; level: number} {
-	const construction = ctx.match[1]
-	const constructions = ctx.session.constructions || {}
-	const level = constructions[construction] || 0
+	const construction: ConstructionName = ctx.match[1]
+	const constructions = ctx.session.constructions as Constructions
+	const level = constructions[construction]
 
 	return {construction, level}
 }
@@ -16,21 +24,32 @@ function constructionFromCtx(ctx: any): {construction: ConstructionName; level: 
 function constructionText(ctx: any): string {
 	const {construction, level} = constructionFromCtx(ctx)
 
-	const text = infoHeader(ctx, construction, level)
+	const requiredResources = calcBuildingCost(construction, level)
+	const currentResources = ctx.session.resources as Resources
 
-	return text
+	const textParts = []
+	textParts.push(infoHeader(ctx, construction, level))
+	textParts.push(constructionResources(ctx, requiredResources, currentResources))
+
+	return textParts.join('\n\n')
 }
 
 menu.button((ctx: any) => `⬆️ ${ctx.wd.label('action.upgrade')}`, 'upgrade', {
-	doFunc: (ctx: any) => {
-		if (!ctx.session.constructions) {
-			ctx.session.constructions = {}
-			for (const c of CONSTRUCTIONS) {
-				ctx.session.constructions[c] = 0
-			}
-		}
-
+	hide: (ctx: any) => {
+		const constructions = ctx.session.constructions as Constructions
 		const {construction, level} = constructionFromCtx(ctx)
+		const requiredResources = calcBuildingCost(construction, level)
+		const currentResources = ctx.session.resources as Resources
+
+		const minutes = calcMinutesNeeded(requiredResources, constructions, currentResources)
+		return minutes > 0
+	},
+	doFunc: (ctx: any) => {
+		const {construction, level} = constructionFromCtx(ctx)
+		const requiredResources = calcBuildingCost(construction, level)
+		const currentResources = ctx.session.resources as Resources
+
+		ctx.session.resources = calcResourcesAfterConstruction(currentResources, requiredResources)
 		ctx.session.constructions[construction] = level + 1
 	}
 })
