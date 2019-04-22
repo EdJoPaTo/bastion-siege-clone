28bf3c5e667926232930db42ebcd6139da08c3c1
Edgar Toll <edjopato@gmail.com>
2019-04-22 12:25:58 +0200

    fix(mystics): logging

--- source/mystics-attacking.ts
+++ source/mystics-attacking.ts
@@ -66,6 +66,7 @@ async function tryAttack(telegram: Telegram): Promise<void> {
 		const languageCode = session.wikidataLanguageCode || 'en'
 
 		const battleResult = calcBattle(qNumber, session)
+		console.log('after mystics battle', user, currentHealth, calcBallistaDamage(session.constructions))
 		const {won, gold, townhall} = battleResult
 
 		let text = ''
